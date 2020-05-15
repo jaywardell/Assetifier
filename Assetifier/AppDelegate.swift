c5f270ffc57c926ed740f4bad14b290070416bdb
Joseph Wardell <jaywardell@mac.com>
2020-05-15 14:21:17 -0400

    Initial Commit

---
+++ Assetifier/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Assetifier
+//
+//  Created by Joseph A. Wardell on 5/15/20.
+//  Copyright © 2020 Joseph A. Wardell. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Assetifier/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Assetifier
+//
+//  Created by Joseph A. Wardell on 5/15/20.
+//  Copyright © 2020 Joseph A. Wardell. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
